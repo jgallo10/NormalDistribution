514bfc21b4d26bce0a662e4fbb12029f9ccf0a04
Jason Matthew Gallo <jgallo@uab.edu>
2022-03-11 03:38:39 -0600

    function for Normal Deviation

---
+++ NormDistEx.py
@@ -0,0 +1,13 @@
+from random import random
+import numpy as np
+
+def normalDist(m, sd, num):
+    num = np.random.normal(m, sd, num)
+    return num
+
+#me = mean, standard Deviation = standDev, num = range number
+me = 5.0
+standDev = 0.1
+num = 10
+
+print(normalDist(me, standDev, num))
