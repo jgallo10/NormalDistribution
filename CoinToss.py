18c7c01866666b90f87b6c7034760f9fd0bc1e59
Jason Matthew Gallo <jgallo@uab.edu>
2022-03-21 22:34:29 -0500

    adding coin toss data and histogram code

---
+++ CoinToss.py
@@ -0,0 +1,22 @@
+import numpy as np
+from pyparsing import nums
+from scipy.stats import norm
+import matplotlib.pyplot as plt
+import math
+'''import pandas as pd
+
+ef = pd.read_excel(r'CoinToss.xlsx')
+print(ef)'''
+
+data=[4,2,6,5,7,7,6,8,5,5,5,4,6,5,2,6,4,6,4,8,4,5,3,3,3]
+
+mu, std = norm.fit(data)
+plt.hist(data, bins=5,density=True,alpha=0.5,color='b')
+
+xmin, xmax = plt.xlim()
+x = np.linspace(xmin, xmax, 100)
+p = norm.pdf(x, mu, std)
+plt.plot(x, p, 'k', linewidth=2)
+title = "Coint Tossing: mu = %.2f, std = %.2f" %(mu, std)
+plt.title(title)
+plt.show()
